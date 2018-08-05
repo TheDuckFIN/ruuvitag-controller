07854b1f29da42e6c69b8f48291e7f3821a7dff6
Valtteri Lakaniemi <valtteri.lakaniemi@reaktor.com>
2018-08-05 19:17:42 +0300

    add more hard-coded names

--- src/tag-store.js
+++ src/tag-store.js
@@ -3,7 +3,10 @@ const foundTags = {}
 const knownTags = {
   '8ac2447cd1ee435e9f73eaa162cd3cee': 'Parveke',
   '0b111d2ab40643a0b8cc0145ca44310f': 'Makkari',
-  '362f5cffb4fa43e5afc49701c748984c': 'Olkkari'
+  '362f5cffb4fa43e5afc49701c748984c': 'Olkkari',
+  'ecbad78f7548': 'Olkkari',
+  'de4defcf927b': 'Makkari',
+  'dadd2e98d559': 'Parveke'
 }
 
 const saveTagData = (tagData) => {
